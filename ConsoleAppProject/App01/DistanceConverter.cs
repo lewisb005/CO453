c3f1e6ce4d1a6c3b7097f519b8f48e653ff831d5
Lewisb005 <100163813@student.burnley.ac.uk>
2024-01-26 14:30:33 +0000

    update

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -6,9 +6,40 @@ namespace ConsoleAppProject.App01
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Lewis version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+
+        private double feet;
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public void Run()
+        { 
+
+        }
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number
+        /// </summary>
+
+        private void InputMiles()
+        {
+
+        }
+        private void CalculateFeet()
+        {
+
+        }
+        private void OutputFeet()
+        {
+
+        }
+
+
     }
 }
